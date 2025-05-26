b6a98823347e270b51110fd66058ce0f4abee092
omrishi123 <omrishi9608@gmail.com>
2025-05-26 19:20:48 +0530

    Initial commit

--- app.py
+++ app.py
@@ -35,18 +35,20 @@ def get_formats():
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                 'Accept': '*/*',
-                'Accept-Language': 'en-US,en;q=0.9',
+                'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+                'Accept-Language': 'en-us,en;q=0.5',
+                'Accept-Encoding': 'gzip, deflate',
                 'Origin': 'https://www.youtube.com',
                 'Referer': 'https://www.youtube.com/',
-                'Sec-Fetch-Dest': 'empty',
-                'Sec-Fetch-Mode': 'cors',
-                'Sec-Fetch-Site': 'cross-site',
+                'Connection': 'keep-alive',
             },
             'extractor_args': {
                 'youtube': {
-                    'player_client': ['web', 'android'],
+                    'player_client': ['android', 'web'],
+                    'player_skip': ['webpage', 'config', 'js'],
                 }
-            }
+            },
+            'socket_timeout': 30,
         }
 
         # Try to use cookies file if it exists
